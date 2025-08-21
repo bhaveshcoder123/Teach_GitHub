ce35dbad62bb01fdc879f29c79dfe3bf5cda248c
Your Name <youremail@example.com>
2025-08-21 22:13:25 +0530

    Dag Correct

--- Graphs/Shortest_Path_DAG.cpp
+++ Graphs/Shortest_Path_DAG.cpp
@@ -1,30 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// User function Template for C++
 class Solution {
   public:
-    // Function to find the shortest path from source to all other nodes
-    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+    
+    vector<int> topoSort(vector<vector<pair<int,int>>> &adj) {
         int V = adj.size();
-        vector<int> ans(V , INT_MAX);
-        ans[src] = 0;
-        queue<int> q;
-        q.push(src);
-        vector<int> visited(V , false);
-        visited[src] = true;
+        vector<int> ans;
+        vector<int> indegree(V);
+        queue<int> q; 
+        for(int i = 0; i<V ; i++) {
+            for(auto& neighbour : adj[i]) {
+                indegree[neighbour.first]++;
+            }
+        }
+        
+        for(int i = 0 ; i<V ; i++) {
+            if(!indegree[i]) q.push(i);
+        }
         
         while(!q.empty()) {
             int node = q.front();
             q.pop();
-            for(int neighbour : adj[node]) {
-                if(!visited[neighbour]) {
-                    ans[neighbour] = min(ans[neighbour] , ans[node] + 1);
-                    visited[neighbour] = true;
-                    q.push(neighbour);
+            ans.push_back(node);
+            
+            for(auto &neighbour : adj[node]) {
+                indegree[neighbour.first]--;
+                if(!indegree[neighbour.first]) q.push(neighbour.first);
+            }
+        }
+        
+        return ans;
+    }
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto &edge : edges){
+            adj[edge[0]].push_back({edge[1] , edge[2]});
+        }
+        
+        vector<int> topoSortArr = topoSort(adj);
+        vector<int> distance(V , INT_MAX);
+        distance[0] = 0;
+        
+        for(int i = 0 ; i<V ; i++) {
+            if(distance[i] != INT_MAX) {
+                for(auto &neighbour : adj[i]) {
+                    int node = neighbour.first;
+                    int dist = neighbour.second;
+                    distance[node] = min(distance[node] , distance[i] + dist);
                 }
             }
         }
-        for(int i = 0 ; i<V ; i++) if(ans[i] == INT_MAX) ans[i] = -1;
-        return ans;
+        for(int i = 0 ; i<V ; i++) if(distance[i] == INT_MAX) distance[i] = -1;
+        return distance;
+        
     }
 };
